1869b50a2e48cfda6dcd29b7761b7afb148cacd2
Marc Byndas <byndasma@gmail.com>
2024-05-08 21:18:55 -0400

    models spaDay app refactored with model to begin studio

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,50 +11,17 @@ import java.util.ArrayList;
 @Controller
 public class SpaDayController {
 
-    @GetMapping("/")
-    public String HomePageForm() { return "index"; }
-
-    public boolean checkSkinType(String skinType, String facialType) {
-        switch(skinType) {
-            case "oily":
-                return facialType.equals("Microdermabrasion")
-                    || facialType.equals("Rejuvenating");
-            case "combination":
-                return facialType.equals("Microdermabrasion")
-                    || facialType.equals("Rejuvenating")
-                    || facialType.equals("Enzyme Peel");
-            case "dry":
-                return facialType.equals("Rejuvenating")
-                    || facialType.equals("Hydrofacial");
-            default: return true;
-        }
+    @GetMapping
+    public String customerForm () {
+        return "index";
     }
 
-    @PostMapping("/menu")
-    public String spaMenu(
-        @RequestParam String name,
-        @RequestParam String skinType,
-        @RequestParam String manipedi,
-        Model model
-    ) {
-        ArrayList<String> facials = new ArrayList<>();
-        facials.add("Microdermabrasion");
-        facials.add("Hydrofacial");
-        facials.add("Rejuvenating");
-        facials.add("Enzyme Peel");
+    @PostMapping
+    public String spaMenu(@RequestParam String skinType, @RequestParam String manipedi, Model model) {
 
-        ArrayList<String> appropriateFacials = new ArrayList<>();
-        for (String facial : facials) {
-            if (checkSkinType(skinType, facial)) {
-                appropriateFacials.add(facial);
-            }
-        }
-
-        model.addAttribute("appropriateFacials", appropriateFacials);
-
-        model.addAttribute("name", name);
-        model.addAttribute("skinType", skinType);
-        model.addAttribute("manipedi", manipedi);
+        Client newClient = new Client(skinType, manipedi);
+        newClient.setAppropriateFacials(skinType);
+        model.addAttribute("client" , newClient);
 
         return "menu";
     }
